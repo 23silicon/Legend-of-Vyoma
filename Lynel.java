c4c51ea5d0be7c6aad67ffbaae829ac56d60d30e
23silicon <45129680+23silicon@users.noreply.github.com>
2022-05-15 22:32:55 -0400

    Create Lynel.java

---
+++ Lynel.java
@@ -0,0 +1,10 @@
+package enemies;
+import main_classes.Enemy;
+@SuppressWarnings("serial")
+public class Lynel extends Enemy {
+
+	public Lynel(String type, int px, int py) {
+		super(type, px, py);
+	}
+
+}
